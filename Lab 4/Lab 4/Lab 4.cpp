92d41bc6798f30b06cd28e36f7c577581da912a1
Nick <mrdoggydog37@gmail.com>
2023-04-07 15:53:44 +0300

    Finished tasks 2-3

--- Lab 4/Lab 4/Lab 4.cpp
+++ Lab 4/Lab 4/Lab 4.cpp
@@ -1,23 +1,55 @@
-#include <iostream>
+﻿#include <iostream>
+#include <iomanip>
+#include <ctime>
 #include <string>
+#include "Levenshtein.h"
 
 int main()
 {
 	srand(time(NULL));
-	std::string s1 = "";
-	for (int i = 0; i < 300; i++)
+
+	std::string S1 = "";
+	for (int i = 0; i < 2500; i++)
 	{
-		s1 += ('a' + rand() % 26);
+		S1 += ('a' + rand() % 26);
 	}
 
-	std::string s2 = "";
-	for (int i = 0; i < 200; i++)
+	std::string S2 = "";
+	for (int i = 0; i < 2500; i++)
 	{
-		s2 += ('a' + rand() % 26);
+		S2 += ('a' + rand() % 26);
 	}
 
-	std::cout << "s1: " << s1 << std::endl << std::endl;
-	std::cout << "s2: " << s2 << std::endl;
+	std::cout << "S1: " << S1 << std::endl;
+	std::cout << "S2: " << S2 << std::endl;
+	std::cout << std::endl;
+
+	clock_t t1 = 0, t2 = 0, t3, t4;
+	/*const int count = 12;
+	double k[count] = { 1. / 10, 1. / 5, 1. / 4, 1. / 3, 4. / 10, 5. / 10, 6. / 10, 7. / 10, 8. / 10, 9. / 10, 1. };*/
+	const int count = 7;
+	double k[count] = { 1.0 / 25, 1.0 / 20, 1.0 / 15, 1.0 / 10, 1.0 / 5, 1.0 / 2, 1 };
+	int lx = S1.length(), ly = S2.length();
+
+	for (int i = 0; i < count; i++)
+	{
+		std::cout << "String length: " << std::setw(6) << (int)(lx * k[i]) << "/" << (int)(ly * k[i]);
+		/*std::cout << "\tRecursion distance: ";
+		std::cout << levenshtein_r(lx * k[i], S1, ly * k[i], S2);*/
+		std::cout << "\tDyn. prog. distance: ";
+		std::cout << std::setw(6) << levenshtein(lx * k[i], S1, ly * k[i], S2);
+		
+		/*t1 = clock();
+		levenshtein_r(lx * k[i], S1, ly * k[i], S2);
+		t2 = clock();
+
+		std::cout << "\tRecursion time: " << (t2 - t1) << " ms";*/
+
+		t3 = clock();
+		levenshtein(lx * k[i], S1, ly * k[i], S2);
+		t4 = clock();
+		std::cout << "\tDyn. prog. time: " << (t4 - t3) << " ms" << std::endl;
+	}
+
+	return 0;
 }
-
-    
---
+++ Lab 4/Lab 4/Levenshtein.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+int levenshtein(
+	int lx,            // длина слова x 
+	std::string x,     // слово длиной lx
+	int ly,            // длина слова y
+	std::string y      // слово y
+);
+// -- дистанции   Левенштeйна (рекурсия)
+int levenshtein_r(
+	int lx,            // длина строки x 
+	std::string x,     // строка длиной lx
+	int ly,            // длина строки y
+	std::string y      // строка y
+);
